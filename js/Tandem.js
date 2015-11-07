e5842f8945177118fdfc298f4a7d009a9d148ea0
samreid <reids@colorado.edu>
2015-11-06 20:50:42 -0700

    updated copyright info using grunt update-copyright-dates, see  https://github.com/phetsims/chipper/issues/403

--- js/Tandem.js
+++ js/Tandem.js
@@ -1,4 +1,4 @@
-// Copyright 2002-2014, University of Colorado Boulder
+// Copyright 2015, University of Colorado Boulder
 
 /**
  * Tandem is a general instance registry that can be used to track creation/disposal of instances in PhET Simulations.
