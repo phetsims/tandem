f8e2cda3c1f11b736a2c29645969c6eee82717cc
samreid <reids@colorado.edu>
2016-02-17 09:45:32 -0700

    Added TandemEmitter and usage for Build an Atom when game over, see https://github.com/phetsims/phet-io/issues/301

---
+++ js/axon/TandemEmitter.js
@@ -0,0 +1,70 @@
+// Copyright 2016, University of Colorado Boulder
+
+/**
+ * Subclass decorator for AXON/Emitter that supplies (mandatory) Tandem registration.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Emitter = require( 'AXON/Emitter' );
+  var tandemNamespace = require( 'TANDEM/tandemNamespace' );
+
+  /**
+   * @param {Object} [options]
+   * @constructor
+   */
+  function TandemEmitter( options ) {
+    assert && assert( options, 'options.tandem must exist' );
+    assert && assert( options.tandem, 'options.tandem must exist' );
+    Emitter.call( this );
+
+    // @private (phet-io)
+    this.callbacksStartedEmitter = new Emitter();
+    this.callbacksEndedEmitter = new Emitter();
+
+    // Tandem registration
+    options.tandem.addInstance( this );
+
+    // @private
+    this.disposeTandemEmitter = function() {
+
+      // Tandem de-registration
+      options.tandem.removeInstance( this );
+    };
+  }
+
+  tandemNamespace.register( 'TandemEmitter', TandemEmitter );
+
+  return inherit( Emitter, TandemEmitter, {
+
+    // @public @override
+    emit: function() {
+      this.callbacksStartedEmitter && this.callbacksStartedEmitter.emit();
+      Emitter.prototype.emit.call( this );
+      this.callbacksEndedEmitter && this.callbacksEndedEmitter.emit();
+    },
+
+    // @public @override
+    emit1: function( arg ) {
+      this.callbacksStartedEmitter && this.callbacksStartedEmitter.emit1( arg );
+      Emitter.prototype.emit1.call( this, arg );
+      this.callbacksEndedEmitter && this.callbacksEndedEmitter.emit();
+    },
+
+    // @public @override
+    emit2: function( arg1, arg2 ) {
+      this.callbacksStartedEmitter && this.callbacksStartedEmitter.emit2( arg1, arg2 );
+      Emitter.prototype.emit2.call( this, arg1, arg2 );
+      this.callbacksEndedEmitter && this.callbacksEndedEmitter.emit();
+    },
+
+    // @public
+    dispose: function() {
+      this.disposeTandemEmitter();
+    }
+  } );
+} );
