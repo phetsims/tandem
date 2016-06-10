28ee43548bd127c2608cc4c94788e7064d74e52b
samreid <reids@colorado.edu>
2016-06-10 11:01:50 -0600

    Converted TNumber to a parametric type and added units as a required argument, see #41

--- js/types/TNumber.js
+++ js/types/TNumber.js
@@ -14,20 +14,24 @@ define( function( require ) {
   var phetioInherit = require( 'PHET_IO/phetioInherit' );
   var TObject = require( 'PHET_IO/types/TObject' );
 
-  var TNumber = phetioInherit( TObject, 'TNumber', function( instance, phetioID ) {
-    TObject.call( this, instance, phetioID );
-    assertTypeOf( instance, 'number' );
-  }, {}, {
-    documentation: 'Wrapper for the built-in JS number type (floating point, but also represents integers)',
+  var TNumber = function( units ) {
+    assert && assert( units, 'All TNumbers should specify units' );
+    return phetioInherit( TObject, 'TNumber', function( instance, phetioID ) {
+      TObject.call( this, instance, phetioID );
+      assertTypeOf( instance, 'number' );
+    }, {}, {
+      units: units,
+      documentation: 'Wrapper for the built-in JS number type (floating point, but also represents integers)',
 
-    fromStateObject: function( stateObject ) {
-      return stateObject;
-    },
+      fromStateObject: function( stateObject ) {
+        return stateObject;
+      },
 
-    toStateObject: function( value ) {
-      return value;
-    }
-  } );
+      toStateObject: function( value ) {
+        return value;
+      }
+    } );
+  };
 
   phetioNamespace.register( 'TNumber', TNumber );
 
