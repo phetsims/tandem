a6a248c2bccc41ad7180119939668cf105285d2a
samreid <reids@colorado.edu>
2016-10-24 12:17:10 -0600

    Added TandemRectangle, see https://github.com/phetsims/phet-io/issues/190

---
+++ js/scenery/nodes/TandemRectangle.js
@@ -0,0 +1,50 @@
+// Copyright 2016, University of Colorado Boulder
+
+/**
+ * Subtype decorator for SCENERY/nodes/Rectangle that supplies (optional) Tandem registration.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
+  var tandemNamespace = require( 'TANDEM/tandemNamespace' );
+  var Tandem = require( 'TANDEM/Tandem' );
+
+  // phet-io modules
+  var TNode = require( 'ifphetio!PHET_IO/types/scenery/nodes/TNode' );
+
+  /**
+   * @param {number} x
+   * @param {number} y
+   * @param {number} width
+   * @param {number} height
+   * @param {Object} [options]
+   * @constructor
+   */
+  function TandemRectangle( x, y, width, height, options ) {
+    Tandem.validateOptions( options ); // The tandem is required when brand==='phet-io'
+    Rectangle.call( this, x, y, width, height, options );
+
+    options.tandem && options.tandem.addInstance( this, TNode ); // TODO: Create TRectangle
+
+    // @private
+    this.disposeTandemRectangle = function() {
+      options.tandem && options.tandem.removeInstance( this );
+    };
+  }
+
+  tandemNamespace.register( 'TandemRectangle', TandemRectangle );
+
+  return inherit( Rectangle, TandemRectangle, {
+
+    // @public
+    dispose: function() {
+      Rectangle.prototype.dispose.call( this );
+      this.disposeTandemRectangle();
+    }
+  } );
+} );
