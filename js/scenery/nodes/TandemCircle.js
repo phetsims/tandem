df61222e2ed1f5564e16d3faebefd56619d381ba
jbphet <john.blanco@colorado.edu>
2016-12-06 13:13:21 -0700

    re-alphabetized the modules after having tandemized it

---
+++ js/scenery/nodes/TandemCircle.js
@@ -0,0 +1,53 @@
+// Copyright 2016, University of Colorado Boulder
+
+/**
+ * Subtype decorator for SCENERY/nodes/Circle that supplies (optional) Tandem registration.
+ *
+ * @author John Blanco (PhET Interactive Simulations)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Circle = require( 'SCENERY/nodes/Circle' );
+  var tandemNamespace = require( 'TANDEM/tandemNamespace' );
+  var Tandem = require( 'TANDEM/Tandem' );
+
+  // phet-io modules
+  var TNode = require( 'ifphetio!PHET_IO/types/scenery/nodes/TNode' );
+
+  /**
+   * @param {number} radius
+   * @param {Object} [options]
+   * @constructor
+   */
+  function TandemRectangle( radius, options ) {
+
+    // Handle new Circle( { radius: ... } )
+    if ( typeof radius === 'object' ) {
+      options = radius;
+    }
+
+    Tandem.validateOptions( options ); // The tandem is required when brand==='phet-io'
+    Circle.apply( this, arguments );
+
+    options.tandem && options.tandem.addInstance( this, TNode ); // TODO: Create TCircle
+
+    // @private
+    this.disposeTandemCircle = function() {
+      options.tandem && options.tandem.removeInstance( this );
+    };
+  }
+
+  tandemNamespace.register( 'TandemRectangle', TandemRectangle );
+
+  return inherit( Circle, TandemRectangle, {
+
+    // @public
+    dispose: function() {
+      Circle.prototype.dispose.call( this );
+      this.disposeTandemCircle();
+    }
+  } );
+} );
