4a6e41d38d4decd8ed11eda3e510959f72ea2d2c
zepumph <michael.kauzmann@colorado.edu>
2017-01-23 13:31:15 -0700

    first pass at logging phetio calls to the event stream, https://github.com/phetsims/phet-io/issues/983

---
+++ js/types/TSimIFrameAPI.js
@@ -0,0 +1,44 @@
+// Copyright 2017, University of Colorado Boulder
+
+/**
+ *
+ * @author - Michael Kauzmann (PhET Interactive Simulations)
+ */
+define( function( require ) {
+  'use strict';
+
+
+  // modules
+  var phetioNamespace = require( 'PHET_IO/phetioNamespace' );
+  var phetioInherit = require( 'PHET_IO/phetioInherit' );
+  var TObject = require( 'PHET_IO/types/TObject' );
+
+  /**
+   *
+   * @param instance
+   * @param phetioID
+   * @constructor
+   */
+  function TSimIFrameAPI( instance, phetioID ) {
+    TObject.call( this, instance, phetioID );
+    assert && assert( false, 'cannot instantiate TSimIFrameAPI' );
+
+
+  }
+
+  phetioInherit( TObject, 'TSimIFrameAPI', TSimIFrameAPI,
+
+    // Instance methods
+    {},
+
+    // Static methods
+    {
+      events: [ 'invoked' ],
+      documentation: 'Instance of a SimIFrameAPI.'
+    }
+  );
+
+  phetioNamespace.register( 'TSimIFrameAPI', TSimIFrameAPI );
+
+  return TSimIFrameAPI;
+} );
