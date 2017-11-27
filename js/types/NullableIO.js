7678f48a4e08d28e9ddc6a14c746c294d670b078
samreid <reids@colorado.edu>
2017-11-27 10:35:23 -0700

    Documented NullableIO, see https://github.com/phetsims/phet-io/issues/1274

--- js/types/NullableIO.js
+++ js/types/NullableIO.js
@@ -1,7 +1,16 @@
 // Copyright 2017, University of Colorado Boulder
 
 /**
+ * Parametric IO Type wrapper that adds support for null values in toStateObject/fromStateObject.
+ * Sample usage:
+ *
+ *  this.ageProperty = new Property( null, {
+ *    tandem: tandem.createTandem( 'ageProperty' ),
+ *    phetioType: PropertyIO( NullableIO( NumberIO ) ) // signifies that the Property can be Number or null
+ * } );
+ *
  * @author Michael Kauzmann (PhET Interactive Simulations)
+ * @author Sam Reid (PhET Interactive Simulations)
  */
 define( function( require ) {
   'use strict';
@@ -11,15 +20,34 @@ define( function( require ) {
   var phetioNamespace = require( 'PHET_IO/phetioNamespace' );
   var ObjectIO = require( 'PHET_IO/types/ObjectIO' );
 
+  /**
+   * Parametric type constructor function, do not use `new`
+   * @param {function} ioType - an IO type (constructor function)
+   * @returns {function} - the IO type that supports null
+   * @constructor
+   */
   function NullableIO( ioType ) {
 
+    // Instantiate the concrete IO type using the specified type parameter
     var NullableIOImpl = function NullableIOImpl( property, phetioID ) {
       ObjectIO.call( this, property, phetioID );
     };
 
     return phetioInherit( ObjectIO, 'NullableIO', NullableIOImpl, {}, {
-      parameterTypes: [ ioType ],
-      documentation: 'A wrapper to wrap another IOType, adding support for null.',
+
+        // Signify parameterTypes to phetioInherit
+        parameterTypes: [ ioType ],
+
+        // Signify documentation, used in documentation wrappers
+        documentation: 'A wrapper to wrap another IOType, adding support for null.',
+
+        /**
+         * Converts the instance to a state object for serialization.
+         * @param {Object|null} instanceOrNull - of type {ioType|null}
+         * @returns {Object|null}
+         * @public
+         * @static
+         */
         toStateObject: function( instanceOrNull ) {
           if ( instanceOrNull === null ) {
             return null;
@@ -29,6 +57,13 @@ define( function( require ) {
           }
         },
 
+        /**
+         * Converts a state object to underlying type if it is non-null.
+         * @param {Object|null} instanceOrNull
+         * @returns {Object|null}
+         * @public
+         * @static
+         */
         fromStateObject: function( instanceOrNull ) {
           if ( instanceOrNull === null ) {
             return null;
@@ -42,7 +77,5 @@ define( function( require ) {
   }
 
   phetioNamespace.register( 'NullableIO', NullableIO );
-
   return NullableIO;
-
 } );
