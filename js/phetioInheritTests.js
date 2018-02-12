8d4bdfb41ac9f247924cc735d174543fd7633f2f
samreid <reids@colorado.edu>
2018-02-12 10:55:19 -0700

    Renamed SimIFrameAPI => phetioCommandProcessor, see https://github.com/phetsims/phet-io/issues/1305

--- js/phetioInheritTests.js
+++ js/phetioInheritTests.js
@@ -9,12 +9,12 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var SimIFrameAPIIO = require( 'PHET_IO/types/SimIFrameAPIIO' );
+  var phetioCommandProcessorIO = require( 'PHET_IO/types/phetioCommandProcessorIO' );
 
   QUnit.module( 'phetioInherit' );
 
   // These tests run in brand=phet-io
   QUnit.test( 'IO types', function( assert ) {
-    assert.ok( !!SimIFrameAPIIO.toStateObject, 'IO types should inherit static properties' );
+    assert.ok( !!phetioCommandProcessorIO.toStateObject, 'IO types should inherit static properties' );
   } );
 } );
