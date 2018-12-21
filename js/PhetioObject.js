ac464815fc5a9667741c70276d10356c13b77f26
zepumph <michael.kauzmann@colorado.edu>
2018-12-20 18:30:41 -0900

    fixed some disposal issues, https://github.com/phetsims/friction/issues/161

--- js/PhetioObject.js
+++ js/PhetioObject.js
@@ -357,6 +357,9 @@ define( function( require ) {
       } );
       this.linkedElements.length = 0;
 
+      this.tandem = null;
+      this.phetioType = null;
+
       this.isDisposed = true;
     }
   }, {
