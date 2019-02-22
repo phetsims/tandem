59d34f0e6c83e8c475a8a24a06487289cffb28dc
samreid <reids@colorado.edu>
2019-02-22 16:06:13 -0700

    Docs

--- js/PhetioObject.js
+++ js/PhetioObject.js
@@ -250,6 +250,8 @@ define( function( require ) {
      *
      * @param {string} event - the name of the event
      * @param {Object|function|null} [data] - data for the event, either an object, or a function that returns an object
+     *                                      - this is transmitted over postMessage using the structured cloning algorithm
+     *                                      - and hence cannot contain functions or other unclonable elements
      * @public
      */
     phetioStartEvent: function( event, data ) {
