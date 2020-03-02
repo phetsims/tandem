7e980a27527c863ca4cc65e1b5b786ae2cefd025
phet-dev <phetadmin@gmail.com>
2020-03-02 03:46:07 -0700

    update copyright dates from daily grunt work

--- js/PhetioIDUtils.js
+++ js/PhetioIDUtils.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2019, University of Colorado Boulder
+// Copyright 2017-2020, University of Colorado Boulder
 
 /**
  * Utilities for creating and manipulating the unique identifiers assigned to instrumented PhET-iO instances, aka
