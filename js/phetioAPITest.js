891090d344311761250f2b532e03ac664c2e8b96
samreid <reids@colorado.edu>
2020-07-15 22:19:34 -0600

    Test API fragments, see https://github.com/phetsims/phet-io/issues/1657

--- js/phetioAPITest.js
+++ js/phetioAPITest.js
@@ -23,7 +23,6 @@ import tandemNamespace from './tandemNamespace.js';
  */
 const phetioAPITest = ( assert, API, componentName, createPhetioObject ) => {
   if ( Tandem.PHET_IO_ENABLED ) {
-    // window.assert && window.assert( ObjectIO.isIOType( IOType ), 'IO Type expected' );
 
     // TODO: remove me when no longer needed, see https://github.com/phetsims/tandem/issues/187
     Tandem.unlaunch();
@@ -53,40 +52,38 @@ const phetioAPITest = ( assert, API, componentName, createPhetioObject ) => {
     const phetioObject = createPhetioObject( expectedComponentTandem );
 
     const visit = ( phetioID, api ) => {
-
+      console.log( phetioID );
       window.assert && window.assert( typeof phetioID === 'string' );
-      window.assert && window.assert( api instanceof Object, 'Object expected for api' );
 
-      let metadata = {};
-      let phetioObject = null;
+      for ( const key in api ) {
+        if ( api.hasOwnProperty( key ) ) {
+          if ( PhetioObject.METADATA_KEYS.includes( key ) || key === 'phetioType' ) {
 
-      // phetioType is a proxy for an "instrumented PhetioObject", we may change that in the future to be any metadata key
-      // TODO: what if SliderIO/SliderAPI is instrumented, but doesn't provide phetioType? https://github.com/phetsims/phet-io/issues/1657
-      if ( api.options && api.options.hasOwnProperty( 'phetioType' ) ) {
+            // Existence of any metadata key indicates the object must be insrumented (and not just an empty intermediate node)
+            const phetioObject = auxiliaryTandemRegistry[ phetioID ];
+            assert.ok( phetioObject, `missing phetioObject for phetioID: ${phetioID}, for phetioType: ${componentName}` );
 
-        phetioObject = auxiliaryTandemRegistry[ phetioID ];
-        assert.ok( phetioObject, `no phetioObject for phetioID: ${phetioID}, for phetioType: ${componentName}` );
-        assert.ok( phetioObject.tandem.phetioID === phetioID,
-          `PhetioObject should be registered with the expected phetioID:  ${expectedComponentTandem.phetioID}` );
-        metadata = phetioObject.getMetadata();
-        assert.equal( phetioObject.phetioType, api.options.phetioType, `Expected phetioType differs for for phetioID: ${phetioID}, phetioType: ${componentName}` );
-      }
-      else {
+            if ( key === 'phetioType' ) {
 
-        // Synthetic elements don't have phetioObjects, but should have children, so make sure that at least one was created.
-        assert.ok( phetioIDs.filter( anyPhetioID => anyPhetioID.startsWith( phetioID ) ).length > 0, `synthetic phetioID expected ${phetioID}` );
-      }
+              // Check for exact type match but also allow subtypes: ChildClass.prototype instanceof ParentClass
+              // https://stackoverflow.com/questions/14486110/how-to-check-if-a-javascript-class-inherits-another-without-creating-an-obj
+              const actualIOType = phetioObject.phetioType;
+              const expectedIOType = api.phetioType;
+              assert.ok( actualIOType === expectedIOType || actualIOType.prototype instanceof expectedIOType, 'Incorrect phetioType' );
+            }
+            else {
 
-      for ( const key in api.options ) {
+              // Presence of a metadataKey indicates it is an instrumented instance
+              assert.equal( phetioObject.phetioFeatured, api[ key ], 'mismatch for ' + phetioID + ':' + key );
+            }
+          }
+          else {
 
-        if ( PhetioObject.METADATA_KEYS.includes( key ) ) {
-          assert.equal( metadata[ key ], api[ key ], `metadata key: ${key} doesn't match desired metadata for phetioID: ${phetioID}` );
-        }
-      }
+            console.log( 'descending to ', key );
 
-      for ( const subKey in api ) {
-        if ( subKey !== 'options' ) {
-          visit( window.phetio.PhetioIDUtils.append( phetioID, subKey ), api[ subKey ] );
+            // Descend to the next level of the api fragment
+            visit( window.phetio.PhetioIDUtils.append( phetioID, key ), api[ key ] );
+          }
         }
       }
     };
