0e428f4ba81e4d22cd3fc4a7e5a9c62a4eed6a2b
zepumph <michael.kauzmann@colorado.edu>
2020-07-24 14:45:08 -0800

    Provide proper phetioType for validation, https://github.com/phetsims/tandem/issues/191

--- js/TandemTests.js
+++ js/TandemTests.js
@@ -6,7 +6,7 @@
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 
-import Property from '../../axon/js/Property.js';
+import NumberProperty from '../../axon/js/NumberProperty.js';
 import Tandem from './Tandem.js';
 
 QUnit.module( 'Tandem', {
@@ -22,25 +22,25 @@ QUnit.module( 'Tandem', {
 
 QUnit.test( 'Tandem validation on ROOT', assert => {
 
-  let p = new Property( 0, {
-    tandem: Tandem.GENERAL.createTandem( 'property' )
+  let p = new NumberProperty( 0, {
+    tandem: Tandem.GENERAL.createTandem( 'aProperty' )
   } );
   assert.ok( p.isPhetioInstrumented(), 'should be instrumented' );
 
-  p = new Property( 0, {
-    tandem: Tandem.GLOBAL.createTandem( 'property' )
+  p = new NumberProperty( 0, {
+    tandem: Tandem.GLOBAL.createTandem( 'aProperty' )
   } );
   assert.ok( p.isPhetioInstrumented(), 'should be instrumented' );
 
-  p = new Property( 0, {
-    tandem: Tandem.GENERAL_VIEW.createTandem( 'property' )
+  p = new NumberProperty( 0, {
+    tandem: Tandem.GENERAL_VIEW.createTandem( 'aProperty' )
   } );
   assert.ok( p.isPhetioInstrumented(), 'should be instrumented' );
 
   // Only specific tandems allowed on root when validating tandems
   window.assert && Tandem.VALIDATION && assert.throws( () => {
-    p = new Property( 0, {
-      tandem: Tandem.GENERAL.createTandem( 'property' )
+    p = new NumberProperty( 0, {
+      tandem: Tandem.GENERAL.createTandem( 'aProperty' )
     } );
   } );
 } );
