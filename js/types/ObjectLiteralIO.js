42832067afa85a02dce1465ba41e6d2c24c251a1
samreid <reids@colorado.edu>
2021-01-20 17:30:38 -0700

    Add ObjectLiteralIO and ValueIO, see https://github.com/phetsims/phet-io/issues/1616

---
+++ js/types/ObjectLiteralIO.js
@@ -0,0 +1,18 @@
+// Copyright 2020, University of Colorado Boulder
+
+import tandemNamespace from '../tandemNamespace.js';
+import IOType from './IOType.js';
+import ValueIO from './ValueIO.js';
+
+/**
+ * IO Type intended for usage with object literals, primarily for toStateObject/fromStateObject.
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+const ObjectLiteralIO = new IOType( 'ObjectLiteralIO', {
+  documentation: 'IO Type for object literals',
+  isValidValue: object => Object.getPrototypeOf( object ) === Object.prototype,
+  supertype: ValueIO
+} );
+
+tandemNamespace.register( 'ObjectLiteralIO', ObjectLiteralIO );
+export default ObjectLiteralIO;
---
+++ js/types/ValueIO.js
@@ -0,0 +1,18 @@
+// Copyright 2020, University of Colorado Boulder
+
+import tandemNamespace from '../tandemNamespace.js';
+import IOType from './IOType.js';
+
+/**
+ * IO Type that uses value semantics for toStateObject/fromStateObject
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+const ValueIO = new IOType( 'ValueIO', {
+  isValidValue: () => true,
+  supertype: IOType.ObjectIO,
+  toStateObject: coreObject => coreObject,
+  fromStateObject: stateObject => stateObject
+} );
+
+tandemNamespace.register( 'ValueIO', ValueIO );
+export default ValueIO;
