73a763e997e075738c121f9ea6f72ce401d5569e
samreid <reids@colorado.edu>
2021-01-29 22:23:04 -0700

    Remove Specification approach, see https://github.com/phetsims/phet-io/issues/1657

--- js/onInstance.js
+++
@@ -1,9 +0,0 @@
-// Copyright 2020, University of Colorado Boulder
-
-import animationFrameTimer from '../../axon/js/animationFrameTimer.js';
-import tandemNamespace from './tandemNamespace.js';
-
-const onInstance = callback => animationFrameTimer.runOnNextTick( callback );
-
-tandemNamespace.register( 'onInstance', onInstance );
-export default onInstance;
