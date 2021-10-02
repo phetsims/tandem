778747670a6ed37a9b31c3e341fc306fb87d8e54
Sam Reid <reids@colorado.edu>
2021-10-01 21:14:58 -0600

    Simplify with ternary, see https://github.com/phetsims/tandem/issues/246

--- js/types/NumberIO.js
+++ js/types/NumberIO.js
@@ -14,24 +14,12 @@ import StateSchema from './StateSchema.js';
 const NumberIO = new IOType( 'NumberIO', {
   valueType: 'number',
   documentation: 'IO Type for Javascript\'s number primitive type',
-  toStateObject: value => {
-    if ( value === Number.POSITIVE_INFINITY ) {
-      return 'POSITIVE_INFINITY';
-    }
-    else if ( value === Number.NEGATIVE_INFINITY ) {
-      return 'NEGATIVE_INFINITY';
-    }
-    return value;
-  },
-  fromStateObject: stateObject => {
-    if ( stateObject === 'POSITIVE_INFINITY' ) {
-      return Number.POSITIVE_INFINITY;
-    }
-    else if ( stateObject === 'NEGATIVE_INFINITY' ) {
-      return Number.NEGATIVE_INFINITY;
-    }
-    return stateObject;
-  },
+  toStateObject: value => value === Number.POSITIVE_INFINITY ? 'POSITIVE_INFINITY' :
+                          value === Number.NEGATIVE_INFINITY ? 'NEGATIVE_INFINITY' :
+                          value,
+  fromStateObject: stateObject => stateObject === 'POSITIVE_INFINITY' ? Number.POSITIVE_INFINITY :
+                                  stateObject === 'NEGATIVE_INFINITY' ? Number.NEGATIVE_INFINITY :
+                                  stateObject,
   stateSchema: StateSchema.asValue( '\'POSITIVE_INFINITY\'|\'NEGATIVE_INFINITY\'|number', {
     isValidValue: value => value === 'POSITIVE_INFINITY' || value === 'NEGATIVE_INFINITY' || ( typeof value === 'number' && !isNaN( value ) )
   } )
