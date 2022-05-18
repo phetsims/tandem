fa5de7a6c527dcc10b4070e5d7029b6b78e1b1f2
zepumph <michael.kauzmann@colorado.edu>
2022-05-18 15:49:06 -0600

    Add API-related types, https://github.com/phetsims/phet-io/issues/1848

--- js/TandemConstants.ts
+++ js/TandemConstants.ts
@@ -12,6 +12,51 @@ import tandemNamespace from './tandemNamespace.js';
 const OBJECT_IO_TYPE_NAME = 'ObjectIO';
 const EVENT_TYPE_MODEL = 'MODEL';
 
+export type PhetioElementData = {
+  initialState: Object;
+}
+
+export type PhetioElement = {
+  _metadata: PhetioObjectMetadata;
+  _data?: PhetioElementData;
+}
+export type PhetioElements = {
+  [ name: string ]: PhetioElements;
+} & PhetioElement;
+
+export type Method = {
+  returnType: string;
+  parameterTypes: string[];
+  documentation: string;
+}
+
+export type PhetioTypes = {
+  [ name: string ]: {
+    methods: Method[];
+    supertype: string;
+    typeName: string;
+    documentation: string,
+    events: string[],
+    metadataDefaults: PhetioObjectMetadata,
+    dataDefaults: Object,
+    methodOrder: string[];
+  }
+}
+
+// Like the generate API files
+export type API = {
+  version: { major: number, minor: number },
+  phetioFullAPI?: boolean;
+  sim: string;
+  phetioElements: PhetioElements;
+  phetioTypes: PhetioTypes;
+}
+
+// Like the old API schema, where keys are the full, dot-separated phetioID
+export type APIFlat = {
+  [ name: string ]: PhetioElement
+}
+
 export type PhetioObjectMetadata = {
 
   // Used in PhetioObjectOptions
