b2eb06ab1887df03c209275f1217862170dd5a2c
Michael Kauzmann <michael.kauzmann@colorado.edu>
2022-12-13 17:30:45 -0700

    rename rule uppercase-to statics-should-be-readonly, https://github.com/phetsims/chipper/issues/1349

--- js/Tandem.ts
+++ js/Tandem.ts
@@ -80,18 +80,18 @@ class Tandem {
   private isDisposed = false;
 
   // Disabling lint rule because GroupTandem is a subtype
-  public static ROOT: RootTandem; // eslint-disable-line statics-should-be-readonly
-  public static ROOT_TEST: Tandem; // eslint-disable-line statics-should-be-readonly
-  public static GENERAL_MODEL: Tandem; // eslint-disable-line statics-should-be-readonly
-  public static GENERAL_VIEW: Tandem; // eslint-disable-line statics-should-be-readonly
-  public static GENERAL_CONTROLLER: Tandem; // eslint-disable-line statics-should-be-readonly
-  public static GLOBAL_MODEL: Tandem; // eslint-disable-line statics-should-be-readonly
-  public static GLOBAL_VIEW: Tandem; // eslint-disable-line statics-should-be-readonly
-  public static COLORS: Tandem; // eslint-disable-line statics-should-be-readonly
-  public static OPTIONAL: Tandem; // eslint-disable-line statics-should-be-readonly
-  public static OPT_OUT: Tandem; // eslint-disable-line statics-should-be-readonly
-  public static REQUIRED: Tandem; // eslint-disable-line statics-should-be-readonly
-  public static PREFERENCES: Tandem; // eslint-disable-line statics-should-be-readonly
+  public static ROOT: RootTandem; // eslint-disable-line uppercase-statics-should-be-readonly
+  public static ROOT_TEST: Tandem; // eslint-disable-line uppercase-statics-should-be-readonly
+  public static GENERAL_MODEL: Tandem; // eslint-disable-line uppercase-statics-should-be-readonly
+  public static GENERAL_VIEW: Tandem; // eslint-disable-line uppercase-statics-should-be-readonly
+  public static GENERAL_CONTROLLER: Tandem; // eslint-disable-line uppercase-statics-should-be-readonly
+  public static GLOBAL_MODEL: Tandem; // eslint-disable-line uppercase-statics-should-be-readonly
+  public static GLOBAL_VIEW: Tandem; // eslint-disable-line uppercase-statics-should-be-readonly
+  public static COLORS: Tandem; // eslint-disable-line uppercase-statics-should-be-readonly
+  public static OPTIONAL: Tandem; // eslint-disable-line uppercase-statics-should-be-readonly
+  public static OPT_OUT: Tandem; // eslint-disable-line uppercase-statics-should-be-readonly
+  public static REQUIRED: Tandem; // eslint-disable-line uppercase-statics-should-be-readonly
+  public static PREFERENCES: Tandem; // eslint-disable-line uppercase-statics-should-be-readonly
 
   public static readonly SCREEN_TANDEM_NAME_SUFFIX = 'Screen';
 
