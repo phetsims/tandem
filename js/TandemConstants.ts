4df5fdd56a179389c0b0587833dac892123b4077
phet-dev <phetadmin@gmail.com>
2023-01-20 04:55:32 -0700

    update copyright dates from daily grunt work

--- js/TandemConstants.ts
+++ js/TandemConstants.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2022, University of Colorado Boulder
+// Copyright 2021-2023, University of Colorado Boulder
 
 import IntentionalAny from '../../phet-core/js/types/IntentionalAny.js';
 import PickRequired from '../../phet-core/js/types/PickRequired.js';
