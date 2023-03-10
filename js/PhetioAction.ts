e4218754d617d66be5a897f05ad1830c45de042c
phet-dev <phetadmin@gmail.com>
2023-03-10 05:22:08 -0700

    update copyright dates from daily grunt work

--- js/PhetioAction.ts
+++ js/PhetioAction.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 /**
  * An instrumented class that wraps a function that does "work" that needs to be interoperable with PhET-iO.
