94780b82fccfe28dd89a8e7ea7d6ac07adde665c
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-06-09 09:46:05 -0600

    add defaultToStateObject tests, https://github.com/phetsims/tandem/issues/297
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- js/types/IOTypeTests.ts
+++ js/types/IOTypeTests.ts
@@ -6,15 +6,45 @@
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 
-import Tandem from '../Tandem.js';
+import IOType from './IOType.js';
+import BooleanIO from './BooleanIO.js';
+import NumberIO from './NumberIO.js';
 
 QUnit.module( 'IOType' );
 
 QUnit.test( 'always true', assert => {
   assert.ok( true, 'initial test' );
 } );
+QUnit.test( 'default toStateObject and applyState', assert => {
 
-if ( Tandem.PHET_IO_ENABLED ) {
+  class MyClass {
+    public firstField = true;
+    public secondField = 5;
+    public willBePrivateInStateObject = 42;
+    private _myUnsettableField = 'unacceptable!';
+    public get myUnsettableField() { return this._myUnsettableField; }
 
-  // Tests would be nice
-}
+    public static MyClassIO = new IOType( 'MyClassIO', {
+      valueType: MyClass,
+      stateSchema: {
+        firstField: BooleanIO,
+        secondField: NumberIO,
+        _willBePrivateInStateObject: NumberIO
+
+        // TODO: this should work with https://github.com/phetsims/tandem/issues/297
+        // myUnsettableField: StringIO
+      }
+    } );
+  }
+
+  const x = new MyClass();
+  const stateObject = MyClass.MyClassIO.toStateObject( x );
+  assert.ok( stateObject.firstField === true, 'stateObjet firstField' );
+  assert.ok( stateObject.secondField === 5, 'stateObjet secondField' );
+  assert.ok( stateObject._willBePrivateInStateObject === 42, 'stateObjet willBePrivateInStateObject' );
+
+  // TODO: this should work with https://github.com/phetsims/tandem/issues/297
+  // assert.ok( stateObject.myUnsettableField === 'unacceptable!', 'stateObjet myUnsettableField' );
+
+  // TODO: defaultApplyStateTests, https://github.com/phetsims/tandem/issues/297
+} );
