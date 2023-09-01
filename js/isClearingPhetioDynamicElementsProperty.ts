54c3e4a3d1cfd4a9be72660a21ec3b588d12d924
Sam Reid <reids@colorado.edu>
2023-09-01 13:09:33 -0600

    Eliminate redundant local attribute, see https://github.com/phetsims/phet-io/issues/1906

--- js/isClearingPhetioDynamicElementsProperty.ts
+++ js/isClearingPhetioDynamicElementsProperty.ts
@@ -2,6 +2,11 @@
 
 /**
  * Property that is set to true when the PhET-iO State Engine is clearing dynamic elements.
+ *
+ * Marking if we are clearing dynamic elements from instrumented containers. This information is useful because certain
+ * logic depends on whether we are setting PhET-iO state but also needs to know about when clearing dynamic elements
+ * to handle it separately.
+ *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
