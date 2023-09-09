681e14c5d8634f445eef1f38b8979cad3d82a626
phet-dev <phetadmin@gmail.com>
2023-09-09 03:20:11 -0600

    update copyright dates from daily grunt work

--- js/types/NullableIO.ts
+++ js/types/NullableIO.ts
@@ -1,4 +1,4 @@
-// Copyright 2018-2022, University of Colorado Boulder
+// Copyright 2018-2023, University of Colorado Boulder
 
 /**
  * Parametric IO Type that adds support for null values in toStateObject/fromStateObject. This type is to
