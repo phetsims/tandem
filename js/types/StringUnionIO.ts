8b679518c8f20b6ef06b4aa1febe5c6fc57e2736
Sam Reid <reids@colorado.edu>
2023-09-12 15:41:58 -0600

    Move TODO from https://github.com/phetsims/tandem/issues/303 to https://github.com/phetsims/tandem/issues/306

--- js/types/StringUnionIO.ts
+++ js/types/StringUnionIO.ts
@@ -27,7 +27,7 @@ const StringUnionIO = <ParameterType extends readonly string[]>( unionValues: Pa
       toStateObject: _.identity,
       fromStateObject: _.identity,
 
-      // TODO: This is the only place that has IO suffix in StateSchema.asValue, and likely change "<" to "(", https://github.com/phetsims/tandem/issues/303
+      // TODO: This is the only place that has IO suffix in StateSchema.asValue, see https://github.com/phetsims/tandem/issues/306
       stateSchema: StateSchema.asValue( `StringUnionIO<${typeName}>`, {
           isValidValue: value => unionValues.includes( value )
         }
