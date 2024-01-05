7b2220bf735d1a615c5fd247015ac2c45e1ded94
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-01-05 12:23:02 -0700

    add author annotations (lint fix), https://github.com/phetsims/chipper/issues/1414

--- js/types/GetSetButtonsIO.ts
+++ js/types/GetSetButtonsIO.ts
@@ -4,6 +4,7 @@ import tandemNamespace from '../tandemNamespace.js';
 
 /**
  * "Marker" style parent class that indicates Studio is supposed to show the Get/Set buttons.
+ * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 const GetSetButtonsIO = new IOType( 'GetSetButtonsIO', {
   isValidValue: ( value: unknown ) => true
