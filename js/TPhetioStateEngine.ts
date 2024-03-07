074163eafcec63c24e0ecf287e6d491953885ccc
phet-dev <phetadmin@gmail.com>
2024-03-07 03:23:57 -0700

    update copyright dates from daily grunt work

--- js/TPhetioStateEngine.ts
+++ js/TPhetioStateEngine.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * The PhetioStateEngine is defined in the phet-io/ repo, so is not available to developers that cannot clone that repo.
