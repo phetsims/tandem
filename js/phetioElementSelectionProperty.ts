de5d5ef0d3aaee34e8febc22fe8a083f58cd9479
phet-dev <phetadmin@gmail.com>
2024-03-23 03:29:02 -0600

    update copyright dates from daily grunt work

--- js/phetioElementSelectionProperty.ts
+++ js/phetioElementSelectionProperty.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * Property that controls the selection view of PhET-iO Elements, predominately in Studio.
--- js/phetioElementsDisplayProperty.ts
+++ js/phetioElementsDisplayProperty.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 import Property from '../../axon/js/Property.js';
 import Tandem from './Tandem.js';
