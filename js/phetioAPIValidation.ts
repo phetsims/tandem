88eb7d0bb070bf1287e21543e3bfc94c28788fa5
phet-dev <phetadmin@gmail.com>
2024-05-18 03:24:22 -0600

    update copyright dates from daily grunt work

--- js/phetioAPIValidation.ts
+++ js/phetioAPIValidation.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2023, University of Colorado Boulder
+// Copyright 2019-2024, University of Colorado Boulder
 
 /**
  * This singleton is responsible for ensuring that the PhET-iO API is correct through the lifetime of the simulation.
