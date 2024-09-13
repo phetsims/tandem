b7e4fddb5cc8a20459f3431a08b246375b62e956
phet-dev <phetadmin@gmail.com>
2024-09-13 03:56:07 -0600

    update copyright dates from daily grunt work

--- js/isPhetioStateEngineManagingPropertyValuesProperty.ts
+++ js/isPhetioStateEngineManagingPropertyValuesProperty.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * Property that is set to true when the PhET-iO State Engine is managing Property values, see ReadOnlyProperty.set()
