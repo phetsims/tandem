30f5fbd4cdf798c438752a72517fd95bcd004c32
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-12-11 17:19:51 -0700

    remove PhetioIDUtils as a preload, https://github.com/phetsims/tandem/issues/316

--- js/PhetioIDUtilsModule.ts
+++ js/PhetioIDUtilsModule.ts
@@ -2,6 +2,7 @@
 
 /**
  * TODO: Don't use this file, convert the UMD into a module, https://github.com/phetsims/tandem/issues/316
+ * TODO: Add to namespace? https://github.com/phetsims/tandem/issues/316
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 
