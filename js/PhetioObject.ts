92759e01ba8a1db2800c6739dd343e110ca59d97
phet-dev <phetadmin@gmail.com>
2025-01-11 03:52:35 -0700

    update copyright dates from daily grunt work

--- js/PhetioObject.ts
+++ js/PhetioObject.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2024, University of Colorado Boulder
+// Copyright 2017-2025, University of Colorado Boulder
 
 /**
  * Base type that provides PhET-iO features. An instrumented PhetioObject is referred to on the wrapper side/design side
