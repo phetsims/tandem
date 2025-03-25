6a8aee8318e5b48ab455fe309fca231486a2bd81
phet-dev <phetadmin@gmail.com>
2025-03-25 03:32:36 -0600

    update copyright dates from daily grunt work

--- js/phet-io-types.ts
+++ js/phet-io-types.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2024, University of Colorado Boulder
+// Copyright 2021-2025, University of Colorado Boulder
 
 /**
  * General TypeScript types that apply to PhET-iO features and architecture.
